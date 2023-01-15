af0128a1bc77235188595c94ec12ca315a31f6ef
Gustavo Gonçalves <gustavofabgoncalves@gmail.com>
2023-01-15 17:51:51 -0300

    Criando a classe PosicaoXadrez

--- CSharpCOMPLETO_Section12/Program.cs
+++ CSharpCOMPLETO_Section12/Program.cs
@@ -13,17 +13,20 @@ namespace CSharpCOMPLETO_Section12
                 //Posicao P;
                 //P = new Posicao(3, 4);
                 //Console.WriteLine("Posição: " + P);
+                //Tabuleiro tab = new Tabuleiro(8, 8);
+                //tab.colocarPeca(new Torre(tab, Cor.Preta), new(0, 0));
+                //tab.colocarPeca(new Torre(tab, Cor.Preta), new(1, 3));
+                //tab.colocarPeca(new Rei(tab, Cor.Preta), new(2, 4));
+                //Tela.imprimirTabuleiro(tab);
 
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+                Console.WriteLine(pos);
+                Console.WriteLine(pos.toPosicao());
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new(2, 4));
 
-               
-                Tela.imprimirTabuleiro(tab);
+
             }
-            catch(TabuleiroException e)
+            catch (TabuleiroException e)
             {
                 Console.WriteLine(e.Message);
             }

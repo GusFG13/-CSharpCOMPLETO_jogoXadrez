0dd1857342b3bcce9429de3459149dc696549187
Gustavo Gonçalves <gustavofabgoncalves@gmail.com>
2023-01-14 22:46:57 -0300

    Projeto criado

---
+++ CSharpCOMPLETO_Section12/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpCOMPLETO_Section12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
